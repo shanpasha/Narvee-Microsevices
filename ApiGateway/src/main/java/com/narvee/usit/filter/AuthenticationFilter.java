9058b0dfa9541ef4002335d3ecc92bbd3e03899d
new <new@shan.Dlink>
2023-08-28 19:14:33 +0530

    Api gateway

---
+++ ApiGateway/src/main/java/com/narvee/usit/filter/AuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.narvee.usit.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.narvee.usit.util.JwtTokenUtil;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+	@Autowired
+	private RouteValidator validator;
+
+	@Autowired
+	private JwtTokenUtil jwtUtil;
+
+	public AuthenticationFilter() {
+		super(Config.class);
+	}
+
+	@Override
+	public GatewayFilter apply(Config config) {
+		return ((exchange, chain) -> {
+			if (validator.isSecured.test(exchange.getRequest())) {
+				// header contains token or not
+				if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+					throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Missing authorization header");
+				}
+
+				String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+				if (authHeader != null && authHeader.startsWith("Bearer ")) {
+					authHeader = authHeader.substring(7);
+				}
+				try {
+//                   
+					jwtUtil.validateToken(authHeader);
+
+				} catch (ResponseStatusException e) {
+					throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Incorrect Username or Password");
+				}
+			}
+			return chain.filter(exchange);
+		});
+	}
+
+	public static class Config {
+
+	}
+}
